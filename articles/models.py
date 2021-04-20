6b45f9fda5f86affbecd859755d5a5e85566bd26
Jahid Hiron <namehiron.96@gmail.com>
2021-04-20 12:14:31 +0600

    authentication and authorization

--- articles/models.py
+++ articles/models.py
@@ -10,6 +10,9 @@ class Article(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,)
 
+    class Meta:
+        ordering = ['-date']
+
     def __str__(self):
         return self.title
 
